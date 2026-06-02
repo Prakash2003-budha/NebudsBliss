d4cab8b10da9572cfaabfbf326b4101839de5baa
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-06-02 23:05:03 +0545

    header

--- Frontend/src/components/header/header.tsx
+++ Frontend/src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../img/logo/logo.jpg';
+import logo from '../../img/logo/logoFull.jpg';
 import styles from './header.module.scss';
 
 export default function Header() {
@@ -13,13 +13,21 @@ export default function Header() {
       </div>
       <nav className={styles.nav}>
         <Link to="/" className={styles.navLink}>Home</Link>
-        <Link to="/about" className={styles.navLink}>Products</Link>
-        <Link to="/services" className={styles.navLink}>AboutUs</Link>
+        <div className={styles.categoryDropdown}>
+          <span className={styles.navLink}>Categories</span>
+          <div className={styles.dropdownMenu}>
+            <Link to="/category/earbuds" className={styles.dropdownItem}>Earbuds</Link>
+            <Link to="/category/powerbank" className={styles.dropdownItem}>Powerbanks</Link>
+            <Link to="/category/camera" className={styles.dropdownItem}>Cameras</Link>
+            <Link to="/category/accessories" className={styles.dropdownItem}>Accessories</Link>
+          </div>
+        </div>
+        <Link to="/AboutUs" className={styles.navLink}>About Us</Link>
         <Link to="/contact" className={styles.navLink}>Contact</Link>
       </nav>
       <div className={styles.signButton}>
-        <button className={styles.SignInButton}>Sign In</button>
-        <button className={styles.SignUpButton}>Sign Up</button>
+        <button className={styles.SignUpButton}>Sign In</button>
+        <button className={styles.SignInButton}>Sign Up</button>
       </div>
     </header>
   );
