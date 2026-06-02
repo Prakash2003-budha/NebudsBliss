b245a1df34465891af8685d5fe4d7effa2fd6e75
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-06-02 21:53:59 +0545

    restructuring the folder

--- Frontend/src/components/header/header.tsx
+++ Frontend/src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../../public/img/logo/logo.jpg'; // Path to your logo
-import styles from './header.module.scss'; // Path to your CSS Module
+import logo from '../../img/logo/logo.jpg';
+import styles from './header.module.scss';
 
 export default function Header() {
   return (
@@ -13,10 +13,14 @@ export default function Header() {
       </div>
       <nav className={styles.nav}>
         <Link to="/" className={styles.navLink}>Home</Link>
-        <Link to="/about" className={styles.navLink}>About</Link>
-        <Link to="/services" className={styles.navLink}>Services</Link>
+        <Link to="/about" className={styles.navLink}>Products</Link>
+        <Link to="/services" className={styles.navLink}>AboutUs</Link>
         <Link to="/contact" className={styles.navLink}>Contact</Link>
       </nav>
+      <div className={styles.signButton}>
+        <button className={styles.SignInButton}>Sign In</button>
+        <button className={styles.SignUpButton}>Sign Up</button>
+      </div>
     </header>
   );
 }
