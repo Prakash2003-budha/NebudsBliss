6c4e23ea0a169f79b2bd623335aa91c38146b675
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-06-05 16:21:35 +0545

    working on th eheader

--- Frontend/src/components/header/header.tsx
+++ Frontend/src/components/header/header.tsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../img/logo/logoFull.jpg';
 import styles from './header.module.scss';
 
-// --- Simple Inline SVG Icons ---
 const HamburgerIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M3 12H21M3 6H21M3 18H21" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
