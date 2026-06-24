25bd45cd957f3e896a10778ca54713bd84ced61e
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-06-24 11:17:47 +0545

    wroking on the route

--- Backend/src/modules/Items/item.router.js
+++ Backend/src/modules/Items/item.router.js
@@ -11,7 +11,7 @@ const itemRouter = Router();
 itemRouter.get('/items', itemCtr.getAllItems);
 itemRouter.get('/items/:id', itemCtr.getItemDetail);
 
-itemRouter.post('/items', allowUser("Admin"), bodyValidator(ItemCreateDTO), uploader().array('images', 5), itemCtr.createItem, fileCleanup);
+itemRouter.post('/items', allowUser("Admin"), bodyValidator(ItemCreateDTO), uploader().array('images', 5), itemCtr.createItem);
 itemRouter.patch('/items/:id', allowUser(), bodyValidator(ItemUpdateDTO), uploader().array('images', 5), itemCtr.updateItem);
 itemRouter.delete('/items/:id',allowUser(),itemCtr.deleteItem);
 
